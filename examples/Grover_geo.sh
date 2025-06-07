90a0bee1c45df03239438e057c6d079807ad36bf
QuantumVerification <qswfinland2025@gmail.com>
2025-06-07 15:21:08 +0200

    Update Grover_geo.sh

--- examples/Grover_geo.sh
+++ examples/Grover_geo.sh
@@ -1,3 +1,3 @@
  python3 -m examples.examples -ex grover_geo -n 5 -samples 3000 
- python3 -m examples.examples -ex grover_geo -n 10 -samples 10000 -err 5
- python3 -m examples.examples -ex grover_geo -n 30 -samples 20000 -err 50 -mu 0.5
+ python3 -m examples.examples -ex grover_geo -n 10 -samples 10000 --err 5
+ python3 -m examples.examples -ex grover_geo -n 30 -samples 20000 --err 50 --mu 0.5
